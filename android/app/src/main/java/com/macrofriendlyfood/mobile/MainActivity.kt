6886d5419336c8cfab1b7370ae4fc0fca790dee6
FullStackDev <chenxidev1129@gmail.com>
2025-09-03 02:18:17 +0800

    fixed MainActivity issue

--- android/app/src/main/java/com/macrofriendlyfood/mobile/MainActivity.kt
+++ android/app/src/main/java/com/macrofriendlyfood/mobile/MainActivity.kt
@@ -8,12 +8,6 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    // Switch from splash theme to app theme
-    setTheme(R.style.AppTheme)
-  }
-
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
@@ -26,4 +20,13 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  /**
+   * CRITICAL FIX for react-native-screens:
+   * Override onCreate to pass null as savedInstanceState to prevent fragment restoration.
+   * This fixes the "Screen fragments should never be restored" error.
+   */
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
 }
